021869d383a573d35aa8a12fa3a186be2a5e7092
gflores05 <gabriel.flores0503@gmail.com>
2023-06-21 15:06:30 -0600

    feat(db): slug and code columns for game and match

--- models.py
+++ models.py
@@ -2,9 +2,17 @@ from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from sqlalchemy.dialects.postgresql import JSONB, ARRAY
+from sqlalchemy.dialects.postgresql import JSONB
+from slugify import slugify
 
 Base = declarative_base()
+
+
+def slugify_column(column_name):
+    def slug_column(context):
+        return slugify(context.current_parameters.get(column_name))
+
+    return slug_column
 
 
 class Game(Base):
@@ -12,7 +20,9 @@ class Game(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     name = Column(String, index=True, nullable=False)
-    code = Column(String, unique=True, index=True, nullable=False)
+    slug = Column(
+        String, unique=True, index=True, nullable=False, default=slugify_column("name")
+    )
     configuration = Column(JSONB, default={"enabled": False})
     date_created = Column(DateTime, nullable=False, server_default=func.now())
     date_updated = Column(DateTime, onupdate=func.now())
@@ -34,8 +44,9 @@ class Match(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     game_id = Column(Integer, ForeignKey("game.id"), nullable=False)
-    players = Column(ARRAY(JSONB), nullable=False)
+    players = Column(JSONB, nullable=False)
     state = Column(JSONB)
+    code = Column(String, unique=True, index=True, nullable=False)
     start_date = Column(DateTime, server_default=func.now(), nullable=False)
     end_date = Column(DateTime, onupdate=func.now())
     winner_id = Column(Integer, ForeignKey("player.id"))
